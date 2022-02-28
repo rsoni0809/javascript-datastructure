c5b7406da303534b91fda0523a8961e4ef84f8a9
rsoni0809 <rahulsoni.iiita@gmail.com>
2022-02-28 12:31:51 +0530

    Javascript LinkedList
    This is primarily for Linked List Operation in Javascript

---
+++ js-linked-list/src/linkedListLoop.js
@@ -0,0 +1,17 @@
+  class Node{
+    constructor(value){
+      this.next = null;
+      this.data = value;
+      this.flag = 0;
+    }
+}
+
+class LinkedList{
+  constructor(){
+    this.head = null;
+    this.length = 0;
+  }
+  findLoopInLL(){
+    
+  }
+}
